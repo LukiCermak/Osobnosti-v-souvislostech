4ea666b382b0bbb08f6bd4526a7176144dcd286e
LukiCermak <luki.cermak@seznam.cz>
2026-04-21 20:17:33 +0200

    Delete specialni-pedagogika-osobnosti-app directory

--- specialni-pedagogika-osobnosti-app/src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-export function App() {
-  return (
-    <main style={{ maxWidth: 900, margin: "2rem auto", fontFamily: "system-ui" }}>
-      <h1>Osobnosti v souvislostech</h1>
-      <p>Interaktivní studijní aplikace pro speciální pedagogiku</p>
-    </main>
-  );
-}
